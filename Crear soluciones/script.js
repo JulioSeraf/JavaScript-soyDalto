61ef13612651c9f0a327a1c56ad2b8492d9409e0
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-03-30 09:46:39 -0300

    Capitulo 1 Creado

---
+++ Crear soluciones/script.js
@@ -0,0 +1,68 @@
+let dineroCofla = prompt ('Cualtos dinero tienes Cofla?')
+let dineroRoberto = prompt('Cualtos dinero tienes Roberto?')
+let dineroPedro = prompt( 'Cuantos dinero tienes Pedro?')
+dineroCofla = parseInt(dineroCofla)
+dineroRoberto = parseInt(dineroRoberto)
+dineroPedro = parseInt(dineroPedro)
+//Cofla
+if (dineroCofla >= 0.6 && dineroCofla < 1){
+    alert('Comprate el helado de agua')
+    alert(`La resta este ${dineroCofla - 0.6}`)
+}else if(dineroCofla >=1 && dineroCofla < 1.6){
+    alert('Comprate el helado de Crema')
+    alert(`La resta este ${dineroCofla - 1}`)
+
+}else if(dineroCofla >=1.6 && dineroCofla < 1.7){
+    alert('Comprete el helado de Heladix')
+    alert(`La resta este ${dineroCofla - 1.6}`)
+}else if (dineroCofla >= 1.7 && dineroCofla < 1.8){
+    alert('Comprate el Helado de Heladovich')
+    alert(`La resta este ${dineroCofla - 1.7}`)
+}else if(dineroCofla >= 2.9){
+    alert('Comprate el helado com confites ou el Pote de 1/4kg')
+    alert(`La resta este ${dineroCofla - 2.9}`)
+}else{
+    alert('NO TE LLEGA EL DINERO CORNO')
+}
+
+//Roberto
+if (dineroRoberto >= 0.6 && dineroRoberto < 1){
+    alert('Comprate el helado de agua')
+    alert(`La resta este ${dineroCofla - 0.6}`)
+}else if(dineroRoberto >=1 && dineroRoberto < 1.6){
+    alert('Comprate el helado de Crema')
+    alert(`La resta este ${dineroCofla - 1}`)
+
+}else if(dineroRoberto >=1.6 && dineroRoberto < 1.7){
+    alert('Comprete el helado de Heladix')
+    alert(`La resta este ${dineroCofla - 1.6}`)
+}else if (dineroRoberto >= 1.7 && dineroRoberto < 1.8){
+    alert('Comprate el Helado de Heladovich')
+    alert(`La resta este ${dineroCofla - 1.7}`)
+}else if(dineroRoberto >= 2.9){
+    alert('Comprate el helado com confites ou el Pote de 1/4kg')
+    alert(`La resta este ${dineroCofla - 2.9}`)
+}else{
+    alert('NO TE LLEGA EL DINERO CORNO')
+}
+//Pedro
+if (dineroPedro
+     >= 0.6 && dineroPedro < 1){
+    alert('Comprate el helado de agua')
+    alert(`La resta este ${dineroCofla - 0.6}`)
+}else if(dineroPedro >=1 && dineroPedro < 1.6){
+    alert('Comprate el helado de Crema')
+    alert(`La resta este ${dineroCofla - 1}`)
+
+}else if(dineroPedro >=1.6 && dineroPedro < 1.7){
+    alert('Comprete el helado de Heladix')
+    alert(`La resta este ${dineroCofla - 1.6}`)
+}else if (dineroPedro >= 1.7 && dineroPedro < 1.8){
+    alert('Comprate el Helado de Heladovich')
+    alert(`La resta este ${dineroCofla - 1.7}`)
+}else if(dineroPedro >= 2.9){
+    alert('Comprate el helado com confites ou el Pote de 1/4kPedro')
+    alert(`La resta este ${dineroCofla - 2.9}`)
+}else{
+    alert('NO TE LLEGA EL DINERO CORNO')
+}
