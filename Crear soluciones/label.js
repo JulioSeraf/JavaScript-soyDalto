50b3e56cd01c72464283ab0442ffcbb270230212
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-04-02 15:15:23 -0300

    Ex de objetos
    orientacion a objetos

--- Crear soluciones/label.js
+++ Crear soluciones/label.js
@@ -1,6 +1,17 @@
-let soma = (num1,num2)=>{
-    let tot = num1 + num2
-    return tot
-}
+class carro{
+    constructor(marca, tamanho, ano){
+        this.marca = marca;
+        this.tamanho = tamanho;
+        this.ano = ano;
+        this.inf = `El nuevo Carro de ${this.ano} de la marca ${this.marca} tiene um tamaño de ${this.tamanho}`
+    }
+    info(){
+        document.write(this.inf + "<br>");
+    }
+} 
 
-console.log(`julio quer somar  ${somador(9,8)}`)
+let mercedea = new carro('Mercedeas',' 2 metros', 2022);
+let honda = new carro('Honda', '1,5 metros', 2021)
+
+mercedea.info();
+honda.info()
