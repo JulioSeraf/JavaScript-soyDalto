a62ce663237865b91e3a0a77060858d3e175345e
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-04-06 12:29:22 -0300

    desafio escuela terminado

--- Crear soluciones/cap7/Escuela/script.js
+++ Crear soluciones/cap7/Escuela/script.js
@@ -3,26 +3,49 @@ const obtenerinfo = (materia)=>{
         fisica: ["Pr:Carlos",'julio',"carlos","maria","jose","paula"],
         matematica:["Pr:Antonio",'julio',"carlos","maria","jose","paula"],
         programacion:["Pra:Josefa",'julio',"carlos","maria","jose","paula"],
-        quimica:["Pr:Golias",'julio',"carlos","maria","jose","paula"],
+        quimica:["Pr:Golias","carlos","maria","jose","paula"],
     }
-    if( materias[materia] !== undefined){
-        return materias[materia];
+    if (materias[materia] == undefined){
+        return materias
     }else{
-        return false;
-    }    
+        return [materias[materia],materia, materias]
+    }
+      
 }
- 
-let res = prompt('Informe a materia:');
-let informacion = obtenerinfo(res);
-let prof = informacion[0];
-let alumnos = informacion;
-alumnos.shift();
+const mostrarinfo = (materia)=>{
+ let informacion = obtenerinfo(materia)
+ let prof = informacion[0][0];
+ let alumnos = informacion[0];
+ alumnos.shift();
+ if(informacion !== false){
+ document.write(`<br>Alunos presente en clase:<strong style ="color:blue;"> ${alumnos}</strong><br> Professor da materia ${informacion[1]}: <strong style ="color :red;">${prof}</strong><br>`);
+ }
+}
 
-if (informacion !== false){
-    document.write(`<strong style = "color:blue;">${prof}</strong> de  <strong style = "color:gray;">${res}</strong> confirma Precencia de los alumnos <strong style = "color:red;">${alumnos}</strong>
-    `);
+const contarinfo = (alumno)=>{
+    let informacion = obtenerinfo();
+    let clasesPresente = [];
+    let cont = 0;
+    for(info in informacion){
+        if(informacion[info].includes(alumno)){
+            cont++
+            clasesPresente.push('<br>' + info);
+        }
+    }
+    if(cont >= 1){
+    return `<br>${alumno} esta em ${cont} clases.<br> Clases com Precencia : ${clasesPresente}`;
+    }else{
+        return `<br> Aluno nao encontrado`
+    }
+    
 }
-else {
-    document.write('Materia nao existe');
-}
+
+
+mostrarinfo('fisica')
+mostrarinfo('programacion')
+mostrarinfo('quimica')
+mostrarinfo('matematica')
+contarinfo('julio')
+document.write(contarinfo('paula'))
+
 
