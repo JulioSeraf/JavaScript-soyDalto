7ea31e746700a48ba616b7a816ff7e39bf870844
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-04-07 06:42:38 -0300

    lista de aulnos terminado

--- Crear soluciones/cap7/lista de alunos/script.js
+++ Crear soluciones/cap7/lista de alunos/script.js
@@ -2,25 +2,36 @@ const clases = (materia)=>{
     let materias = {
         fisica: ['carlos0','maria','jose','antonio','pedro','paula','ana','jessica'],
         matematica: ['carlos0','maria','jose','antonio','pedro','paula','ana','jessica'],
-        programacio: ['carlos0','maria','jose','antonio','pedro','paula','ana','jessica'],
+        programacion: ['carlos0','maria','jose','antonio','pedro','paula','ana','jessica'],
         quimica: ['carlos0','maria','jose','antonio','pedro','paula','ana','jessica','pedro'],
         lengua: ['carlos0','maria','jose','antonio','pedro','paula','ana','jessica','cofla','roberto','clara']
     }
     return materias[materia];
 }
-let alumno = prompt('Benvenido!!? informe o nome:')
-let pergunta = prompt('Qual materia deseas se escrever ?')
-let perg = clases(pergunta)
-if(perg == undefined){
-    document.write('materia nao existe!')
-}else{
-    if(perg.length <= 10){
-        let res = prompt(`Clase com um total de ${perg.length}/10 alumno. Te queres escrever? sim/nao?`)
-        if(res == 'sim' || res == 'SIM'){
-            perg.push(alumno)
-            document.write(`Alunos en clase: <br> <strong  style="color:blue;">${perg}</strong>`)
+function escricion (perg){
+    if(perg.length <=10){
+        let res = prompt(`La materia de ${pergunta} tiene un total de ${perg.length}/10 alumnos. queres escrivir-se? si/no?`);
+        if(res == 'si' || res == 'SI'){
+            perg.push(alumno);
+            document.write(`<br>Benvenido a Clase de <strong>${pergunta}</strong>!! <br> confira os alumno em clase abajo.<br><strong> ${perg}</strong><br>`)
+        }else{
+            alert('OK')
         }
-    }else if(perg.length >= 11){
-        document.write(`Lo sieto <strong>${alumno}</strong>a materia de <strong> ${pergunta} </strong> ja esta completa!`)
     }
+    else{
+        alert('materia ja se encontra completa !!')
+    }
+    
 }
+let alumno = prompt('Benvenido!!? informe o nome:');
+let resposta = 'si';
+do{
+    var pergunta = prompt('Qual materia deseas se escrever');
+    let perg = clases(pergunta);
+    if(perg !== undefined){
+        escricion(perg);
+    }else{
+        alert('Materia nao existe!')
+    }  
+    resposta = prompt('Queres escreviser em outra materia?')  
+}while(resposta == 'si')
