a3851fe3f1356a9ea820f7569f670e4721623c07
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-04-09 14:39:57 -0300

    estudios de childs

---
+++ .vscode/launch.js
@@ -0,0 +1,10 @@
+let contenedor = window.document.querySelector(`.contenedor`);
+
+const item = document.createElement('LI');
+
+const textoDelItem = document.createTextNode('julio')
+ 
+const primerioh = contenedor.firstChild;
+
+
+document.write(primerioh)
