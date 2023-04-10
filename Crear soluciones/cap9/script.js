7bc82cda586c27de90002deeb196bf515805cc8d
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-04-10 04:27:52 -0300

    Creando projeto llave

---
+++ Crear soluciones/cap9/script.js
@@ -0,0 +1,27 @@
+const conterTodo = window.document.querySelector(".flex-container")
+const crearLlave = (nombre,modelo,precio) =>{
+    img = "<img src='llave.png' class='llave-img'>",
+    nombre = `<h2>${nombre}<h2>`;
+    modelo = `<h3>${modelo}</h3>`;
+    precio = `<p>Precio: <strong>${precio}</strong></p>`;
+    return [img,nombre,modelo,precio];
+}
+let documentFragment = document.createDocumentFragment();
+
+for(var i = 0 ; i < 20; i++){
+    
+    let modelorandom = Math.random()*10000;
+    let preciorandom = Math.random()*10+30;
+    let modelo = Math.round(modelorandom);
+    let precio = Math.round(preciorandom);
+    let llave = crearLlave(`llave ${i}`, `Modelo ${modelo}`,`Precio $${precio} `);
+    let div = document.createElement('DIV');
+    div.addEventListener("click",()=>{changeHidden(modelo)});
+    div.tabIndex = i;
+    div.classList.add(`ìtem-${i}`, "flex-item");
+    div.innerHTML += llave[0] + llave[1] + llave[2] + llave[3];
+    documentFragment.appendChild(div);
+
+}
+
+conterTodo.appendChild(documentFragment);
