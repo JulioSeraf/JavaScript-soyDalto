0e7f758aa271e9947f7d8c0a5b1ef58f1d47285a
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-04-14 05:04:20 -0300

    cap10 atualizado

---
+++ nivel medio/cap10/script.js
@@ -0,0 +1,29 @@
+const button = document.querySelector('.button');
+const button1 = document.querySelector('.button1');
+const contenedor = document.querySelector('.contenedor');
+const input = document.getElementById('input-prueva')
+// contenedor.addEventListener('mouseup',()=>{alert('contenedor')})
+
+// button.addEventListener("mousemove", (e)=>{
+//     alert('button')
+// });
+
+// button1.addEventListener("click", ()=>{alert('botao1')});
+// input.addEventListener('keydown', (e)=>{
+//     console.log('una tecla fue precionada')
+// });
+// input.addEventListener('keypress', (e)=>{
+//     console.log('una tecla fue apertada')
+// });
+// input.addEventListener('select', (e)=>{
+//     let start = e.target.selectionStart;
+//     let end = e.target.selectionEnd;
+//     let selecionado = input.value.substring(start,end)
+//     contenedor.innerHTML = `empieza ${start} e terminda em ${end} ${selecionado})}`
+    
+// });
+
+
+addEventListener('load',()=>{
+    console.log('se a cargado el sitio')
+})
