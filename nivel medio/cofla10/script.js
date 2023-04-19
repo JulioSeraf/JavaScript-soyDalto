227f5e6724073cb6e9a03778c55a59e699c2ec3c
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-04-19 09:28:08 -0300

    criando cofla10

---
+++ nivel medio/cofla10/script.js
@@ -0,0 +1,6 @@
+const nota1 = document.querySelector('#inota1');
+const nota2 = document.querySelector('#inota2');
+const nota3 = document.querySelector('#inota3');
+const info = document.getElementById()
+
+document.write(nota1, nota2, nota3)
