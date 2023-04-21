0798781e54f9f28346eedef2af43d7c43759e9e1
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-04-21 06:57:05 -0300

    testando callback

--- text.js
+++ text.js
@@ -1,9 +1,23 @@
-try{
-     throw
+class Persona {
+    constructor(nombre, instagram){
+        this.nombre = nombre;
+        this.instagram = instagram;
+    }
+};
+
+ const datos = [
+    ['jlio','@serfim'],
+    ['carlos','@carlinho'],
+    ['maria','@mariii'],
+    ['jose ','@joselin'],
+    ['ojuara','@desafiador'],
+ ]
+ let personas = [];
+for(persona in datos) {
+    personas[persona] = new Persona(datos[persona][0],datos[persona][1])
 }
-catch(e){
-    console.log(e)
-}
-finally{
+
+const obterPersona = (id,cb) => {
+    
 
 }
