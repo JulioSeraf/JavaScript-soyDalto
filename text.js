38c3bae560de4344dc1519ea6e3e3519d0e1fd0a
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-05-09 11:41:58 -0300

    aprendo callback

--- text.js
+++ text.js
@@ -1,8 +1,42 @@
-function testarray( array){
-    for(i in array){
-        console.log(array[i] * 2);
-    };
-};
-let array = [2,3,4,5,5];
+class persona {
+    constructor(nombre, instagram){
+        this.nombre = nombre;
+        this.instagram = instagram;
+    }
+}
 
-testarray(array);
+let datos = new persona('julio serafim', '@serafim');
+
+const datosPersonas = [
+    ['julio','@serafim'],
+    ['antonio','@antonis'],
+    ['carlos','@carlinhos'],
+    ['maria','@amamama'],
+    ['isabele','@isasoa'],
+    ['sena','@altonsena'],
+    ['ajose ','@ajoseee'],
+];
+let personas = [];
+for ( per in datosPersonas){
+    personas[per] = new persona(datosPersonas[per][0],datosPersonas[per][1])
+
+}
+ const obtenerPersona = (id,cb)=>{
+    if(personas[id] == undefined){
+        cb('No se ha encontrado la persona');
+    }else{
+        cb(null,personas[id])
+    }
+ }
+ obtenerPersona(6,(err,persona)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log(persona.nombre);
+        console.log(persona.instagram)
+    }
+ })
+
+
+ 
+
