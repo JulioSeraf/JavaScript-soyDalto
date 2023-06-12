e033e1a5ae7d772d501c933b5f547d0d7c29170a
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-06-12 16:07:40 +0200

    testando sobre promesas

--- promessas.js
+++ promessas.js
@@ -1,1 +1,11 @@
 
+class persona{
+    constructor(instagran, nombre ){
+        this.nome = nombre;
+        this.instagran = instagran; 
+    }
+}
+const datosPersona =[['carlos','@Carlinhos'],
+[],[],[],[]
+]
+console.log('julio','@juninho')
