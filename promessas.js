459c668a68aca3e1ad3f843e7701e28e86029d92
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-06-12 16:30:19 +0200

    testando callback

--- promessas.js
+++ promessas.js
@@ -1,11 +1,25 @@
 
 class persona{
-    constructor(instagran, nombre ){
+    constructor(nombre,instagran ){
         this.nome = nombre;
         this.instagran = instagran; 
     }
 }
 const datosPersona =[['carlos','@Carlinhos'],
-[],[],[],[]
+['maria', '@mariabel'],
+['antonio','@tonnorel'],
+['Paula','@Paulanela'],
+['maay','@maryyyybell']
 ]
-console.log('julio','@juninho')
+
+const personas = [];
+
+for(i in datosPersona){
+   personas[i] = new persona(datosPersona[i][0],datosPersona[i][1])
+}
+
+function obtenerPersona(nombre){
+    for(i in datosPersona){
+        
+    }
+}
