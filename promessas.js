654e342e92699af1a68f9283ff7a548d45188134
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-06-13 16:06:03 +0200

    testando Callback

--- promessas.js
+++ promessas.js
@@ -18,8 +18,18 @@ for(i in datosPersona){
    personas[i] = new persona(datosPersona[i][0],datosPersona[i][1])
 }
 
-function obtenerPersona(nombre){
-    for(i in datosPersona){
-        
-    }
+function obtenerPersona(id,callback){
+    if(persona[id]== undefined){
+        callback('No se a encontrado la persona!.')
+    }else(
+        callback(null,persona[id])
+    )
 }
+
+obtenerPersona(0, (err, persona)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log(persona)
+    }
+})
